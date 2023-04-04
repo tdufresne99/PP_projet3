f42a5b353c6e37dba8786d46b05b013b60e1904b
Thomas Dufresne <tdufresne99@gmail.com>
2023-04-04 16:57:21 -0400

    Added variable to track each character's room in CurrentRoomManager

--- Assets/Scripts/CurrentRoomManager.cs
+++ Assets/Scripts/CurrentRoomManager.cs
@@ -10,6 +10,12 @@ public class CurrentRoomManager : MonoBehaviour
     [SerializeField] private ObjectCurrentRoom _boyCurrentRoomCS;
     [SerializeField] private ObjectCurrentRoom _girlCurrentRoomCS;
 
+    [SerializeField] private RoomsEnum _playerCurrentRoom;
+    [SerializeField] private RoomsEnum _motherCurrentRoom;
+    [SerializeField] private RoomsEnum _fatherCurrentRoom;
+    [SerializeField] private RoomsEnum _boyCurrentRoom;
+    [SerializeField] private RoomsEnum _girlCurrentRoom;
+
     void Start()
     {
         _playerCurrentRoomCS.OnRoomChanged += OnPlayerChangedRoom;
@@ -22,21 +28,26 @@ public class CurrentRoomManager : MonoBehaviour
     private void OnPlayerChangedRoom(RoomsEnum room)
     {
         Debug.Log("Player is now in " + room);
+        _playerCurrentRoom = room;
     }
     private void OnMotherChangedRoom(RoomsEnum room)
     {
         Debug.Log("Mother is now in " + room);
+        _motherCurrentRoom = room;
     }
     private void OnFatherChangedRoom(RoomsEnum room)
     {
         Debug.Log("Father is now in " + room);
+        _fatherCurrentRoom = room;
     }
     private void OnBoyChangedRoom(RoomsEnum room)
     {
         Debug.Log("Boy is now in " + room);
+        _boyCurrentRoom = room;
     }
     private void OnGirlChangedRoom(RoomsEnum room)
     {
         Debug.Log("Girl is now in " + room);
+        _girlCurrentRoom = room;
     }
 }
